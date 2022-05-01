d2ec0c64241138a8d4bf8e3f68a520868b662421
daniil <trostin1998@bk.ru>
2022-05-01 20:26:43 +0300

    HW 6 new

--- src/ru/test/Main.java
+++ src/ru/test/Main.java
@@ -5,44 +5,40 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(generateRandomArray()));
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
         // Task 1
         int costs = 0;
-        for (int i = 0; i < generateRandomArray().length - 1; i++) {
-            costs = generateRandomArray()[i] + costs;
+        for (int i = 0; i < arr.length - 1; i++) {
+            costs = arr[i] + costs;
         }
         System.out.println("Сумма трат за месяц составила " + costs + " рублей");
 
         // Task 2
         int maxCosts = 0;
         int minCosts = 1_000_000;
-        for (int i = 0; i < generateRandomArray().length; i++) {
-            if (generateRandomArray()[i] > maxCosts) {
-                maxCosts = generateRandomArray()[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxCosts) {
+                maxCosts = arr[i];
             }
-            if (generateRandomArray()[i] < minCosts){
-                minCosts = generateRandomArray()[i];
+            if (arr[i] < minCosts){
+                minCosts = arr[i];
             }
             }
         System.out.println("Минимальная сумма трат за день составила " + minCosts + " рублей. Максимальная сумма трат за день составила " + maxCosts + " рублей.");
         // Task 3
         int allCosts = 0;
-        for (int i = 0; i < generateRandomArray().length; i++) {
-            allCosts = generateRandomArray()[i] + allCosts;
+        for (int i = 0; i < arr.length; i++) {
+            allCosts = arr[i] + allCosts;
         }
         float middleCosts = allCosts / 30;
         System.out.println(middleCosts);
         // Task 4
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        char[] fullName = {'I', 'v', 'a', 'n', 'o', 'v', ' ', 'I', 'v', 'a', 'n'};
-        for (char i = 0; i < fullName.length; i++) {
-            System.out.print(fullName);
-            break;
+        for (int i = reverseFullName.length-1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
-
-    int[] arr = generateRandomArray();
-
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
